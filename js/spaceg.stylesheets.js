5b6842ef8fcb2387b71e1de05883cb790eed5be7
Lucas Gatsas <lucasgatsas@gmail.com>
2015-05-06 23:20:56 +0200

    Update Full Blog ///

--- js/spaceg.stylesheets.js
+++ js/spaceg.stylesheets.js
@@ -1,13 +1,14 @@
 /*!
  Top Stylesheet for my Blog! 
- spaceg.github.io by Lucas Gatsas. 
+ spaceg.github.ch by Lucas Gatsas. 
  */
  /* New Update Mo. 2 Feb. 2015 -02:33:45*/
 /* New Update Fr. 6 Feb. 2015 -01:57:13 */
 /* New Update Sa. 21. February. 2015 -02:58:16*/
 /* New Update Fr. 27. February. 2015 -21:53:19*/
+/* New Update Mo. 04. Mai. 2015 -21:53:19*/ 
 
-
+/* Smoke Weed and Stay High */ 
 
 
  /*!
